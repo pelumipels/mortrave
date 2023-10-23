34e0cf2ff883386ddcfa327e1fff446c0cd45735
Olatunde Emmanuel Oluwapelumi <pelumi6000@gmail.com>
2023-10-23 07:55:09 +0100

    updated the faqs

--- src/components/accordionData.js
+++ src/components/accordionData.js
@@ -1,18 +1,18 @@
 export const accordionData = [
     {
-        question: 'How does Mortrave work?',
-        answer: 'Lorem ipsum dolor sit amet consectetur. Lorem ut sed aliquam vulputate consequat eu vestibulum faucibus morbi. Vel sagittis nibh sed aliquam est elit molestie scelerisque. Sit id adipiscing nunc vel orci. Leo feugiat rhoncus pellentesque vitae eget.'
+        question: "How does Mortrave work?",
+        answer: "If you have any project that needs to be done in Africa, all you need to do is have a Mortrave account, fund your wallet and let us help you with all the funds disbursements. You can also plan and save towards a project by using Mortrave."
     },
     {
-        question: 'Is it safe to use Mortrave?',
-        answer: 'Lorem ipsum dolor sit amet consectetur. Augue lacus consequat volutpat elit. Gravida euismod semper netus lacus. Ultrices tortor viverra sed id placerat. Condimentum fringilla tempus mi blandit in tincidunt condimentum enim vitae. Dignissim.'
+        question: "Is it safe to use Mortrave?",
+        answer: "Absolutely! All the information you provide upon signing up are protected and all the money you give to us to help you pay for projects are used exactly how you want them."
     },
     {
-        question: 'Can I track my transfer?',
-        answer: 'Lorem ipsum dolor sit amet consectetur. Facilisis rutrum ante nulla a vulputate neque bibendum tristique lacinia. Cum eget vestibulum non lobortis et mauris aliquam. Eu leo etiam suspendisse purus morbi sapien. Blandit pulvinar in augue turpis.'
+        question: "Can I track my transfer?",
+        answer: "Yes. From the time you make a payment and the time the necessary bodies get their payments into their local accounts, you will be notified from time to time."
     },
     {
-        question: 'Are there any hidden fees with Mortrave?',
-        answer: 'Lorem ipsum dolor sit amet consectetur. Facilisis rutrum ante nulla a vulputate neque bibendum tristique lacinia. Cum eget vestibulum non lobortis et mauris aliquam. Eu leo etiam suspendisse purus morbi sapien. Blandit pulvinar in augue turpis.'
+        question: "Are there any hidden fees with Mortrave?",
+        answer: "No, there aren’t. There are no hidden fees to pay after the transaction details shown to you per project."
     }
 ];
