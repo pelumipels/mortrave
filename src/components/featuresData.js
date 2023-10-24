2ff20d6d10b19a134bd4e869232975aec55bf925
Olatunde Emmanuel Oluwapelumi <pelumi6000@gmail.com>
2023-10-24 13:37:05 +0100

    Updated features Data

--- src/components/featuresData.js
+++ src/components/featuresData.js
@@ -1,26 +1,26 @@
 export const featuresData = [
     {
-        featuresTopic: 'Say Goodbye to International Payment Hassles',
-        featuresDetail: 'Start your journey to hassle free Cross-Border Payments by downloading the Mortrave app from your iOS of Android store.',
-        featuesButtonText: 'Budgeting Tool',
-        featuresSubDetail: 'With this feature, you can book items ahead of time and make a plan for what you want to do. You also have access to AI tools that enable you to begin, create and record your to-do list.',
+        featuresTopic: "Streamline your global transactions effortlessly",
+        featuresDetail: "Wave farewell to payment complications and launch into a stress-free cross-border financial journey.",
+        featuesButtonText: "Budgeting Tool",
+        featuresSubDetail: "With this feature, you can book items ahead of time and make a plan for what you want to do. You also have access to AI tools that enable you to begin, create and record your to-do list.",
     },
     {
-        featuresTopic: 'Say Goodbye to International Payment Hassles',
-        featuresDetail: 'Start your journey to hassle free Cross-Border Payments by downloading the Mortrave app from your iOS of Android store.',
-        featuesButtonText: 'Add Contact',
-        featuresSubDetail: 'This allows you to create a contact list of important people to help you execute your ideas, this feature allows you to store name, phonebook, account details and address of important go to contacts.',
+        featuresTopic: "Say Goodbye to International Payment Hassles",
+        featuresDetail: "Embark on a seamless cross-border payment experience with ease – simply download the Mortrave app from your iOS or Android Store.",
+        featuesButtonText: "Add Contact",
+        featuresSubDetail: "This allows you to create a contact list of important people to help you execute your ideas, this feature allows you to store name, phonebook, account details and address of important go to contacts.",
     },
     {
-        featuresTopic: 'Say Goodbye to International Payment Hassles',
-        featuresDetail: 'Start your journey to hassle free Cross-Border Payments by downloading the Mortrave app from your iOS of Android store.',
-        featuesButtonText: 'Search',
-        featuresSubDetail: 'With this feature, you can book items ahead of time and make a plan for what you want to do. You also have access to AI tools that enable you to begin, create and record your to-do list.',
+        featuresTopic: "Eliminate the headache of international payments",
+        featuresDetail: "Start your journey to hassle free Cross-Border Payments by downloading the Mortrave app from your iOS of Android store.",
+        featuesButtonText: "Search",
+        featuresSubDetail: "With this feature, you can book items ahead of time and make a plan for what you want to do. You also have access to AI tools that enable you to begin, create and record your to-do list.",
     },
     {
-        featuresTopic: 'Say Goodbye to International Payment Hassles ',
-        featuresDetail: 'Start your journey to hassle free Cross-Border Payments by downloading the Mortrave app from your iOS of Android store.',
-        featuesButtonText: 'Advance Pay',
-        featuresSubDetail: 'You have the ability to set up payments to your key contacts on the dates that you specify for your budget and begin to achieve your goals.',
+        featuresTopic: "Dive into project planning without the fuss",
+        featuresDetail: "Say farewell to complications and kickstart your hassle-free project journey.",
+        featuesButtonText: "Advance Pay",
+        featuresSubDetail: "You have the ability to set up payments to your key contacts on the dates that you specify for your budget and begin to achieve your goals.",
     }
 ]
